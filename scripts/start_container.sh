f81f6239f8fb6d4e417e3fb06fc80782fb093418
VikrantDhokale1 <139742612+VikrantDhokale1@users.noreply.github.com>
2024-06-11 19:06:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -e
 
-
 # Pull the Docker image from Docker Hub
-
-
 docker pull vikranthub/simple-python-flask-app:latest
-
-
-
-
-
-
 # Run the Docker image as a container
-
 docker run -d -p 5000:5000 vikranthub/simple-python-flask-app:latest  
+
 
 
